be4fbeb4dce9450865a92df051c2ad51fbb30793
Owner <braydens.coding@gmail.com>
2024-10-27 19:05:48 -0500

    Exercise [COMPLETED]

---
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -0,0 +1,36 @@
+package org.launchcode.codingevents.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class EventCategory {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Size(min = 3, message = "Name must be at least 3 characters.")
+    private String name;
+
+    public EventCategory(String name) {
+        this.name = name;
+    }
+
+    public EventCategory() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+}
